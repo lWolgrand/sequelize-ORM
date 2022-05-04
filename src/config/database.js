37320a19b151bad66a9d7974a998ea668cdadc1d
Wolgrand Lopes <lopeswolgrand@gmail.com>
2022-05-04 18:24:26 -0300

    Criando Models, Router e Controllers e configurando eles no Index.

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'root',
+    password: 'root',
+    database: 'sequelize',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
