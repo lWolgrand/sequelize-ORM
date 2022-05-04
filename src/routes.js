1d11c55e332d17ee1ef022cff4354c6b49887a19
Wolgrand Lopes <lopeswolgrand@gmail.com>
2022-05-04 19:53:57 -0300

    Configurando Controllers e routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const UserController = require('./controllers/UserController');
 const routes = express.Router();
 
-routes.post('/users', UserController.store);
+routes.get('/users', UserController.index); // Listagem de usuários sendo o index o método que será chamado
+routes.post('/users', UserController.store); // Cadastro de usuários sendo o store o método que será chamado
 
 module.exports = routes;
