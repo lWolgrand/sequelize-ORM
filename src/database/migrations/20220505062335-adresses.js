ad24114b4bbfc04d1eef2b29af3383aac759948e
lWolgrand <lopeswolgrand@gmail.com>
2022-05-05 03:59:04 -0300

    Criando tabela relacional de endereço, (migrations/adresses.js) do usuário. Comando: yarn sequelize db:migrate

---
+++ src/database/migrations/20220505062335-adresses.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('adresses', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { //Para que o usuário seja relacionado com o endereço 
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+      },
+      zipcode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
